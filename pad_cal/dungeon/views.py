8da5bd70253d18a9e4bb41f149d82d293ddc8113
Rohil Thopu <rohilthopu@gmail.com>
2018-06-30 00:58:47 -0500

    implemented parse into a test home view!!

--- pad_cal/dungeon/views.py
+++ pad_cal/dungeon/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Dungeon
 from .forms import DungeonLink
+from bs4 import BeautifulSoup as bs
+import urllib
+
 # Create your views here.
+
+parsedDungeon = {}
+
 
 def homeView(request):
     template = 'home.html'
@@ -9,7 +15,7 @@ def homeView(request):
     source = Dungeon.objects.all()
     form = DungeonLink()
 
-    context = {'dungeons' : source, 'form' : form}
+    context = {'dungeons': source, 'form': form}
 
     if request.method == 'POST':
         form = DungeonLink(request.POST)
@@ -17,13 +23,145 @@ def homeView(request):
             # verify that it doesnt already exist
             data = form.cleaned_data
             exists = False
+
+            link = data.get('dungeonLink')
+
             for item in source:
-                if item.dungeonLink.lower() == data.get('dungeonLink').lower():
+                if item.dungeonLink.lower() == link.lower():
                     exists = True
             if (not exists):
-                dungeon = Dungeon(dungeonLink=data.get('dungeonLink'))
+                parsedDungeon['dungeonLink'] = link
+                parse(link)
+
+                dungeon = Dungeon(jpnTitle=parsedDungeon['jpnTitle'], altTitle=parsedDungeon['altTitle'],
+                                  altTitle2=parsedDungeon['altTitle2'], stamina=parsedDungeon['stamina'],
+                                  battles=parsedDungeon['battles'],
+                                  dungeonLink=parsedDungeon['dungeonLink'], dungeonType=parsedDungeon['dungeonType'])
                 dungeon.save()
                 return redirect('/home/')
             else:
                 return redirect('/home/')
     return render(request, template, context)
+
+
+def parse(link):
+    site = urllib.request.urlopen(link)
+    soup = bs(site, 'lxml')
+    parse_titles(soup)
+    parse_dungeon(soup)
+    # parse_encounters(soup)
+
+
+def parse_titles(soup):
+    # The title of the dungeon
+    title = soup.body.find(class_='name').get_text().strip()
+    # Japanese Title
+    titlej = soup.body.find(class_='jap').get_text()
+    # Alternate Name
+    alt_title = soup.body.find(class_='title value-end nowrap').get_text().strip()
+
+    parsedDungeon['jpnTitle'] = titlej
+    parsedDungeon['altTitle'] = alt_title
+    parsedDungeon['altTitle2'] = title
+
+    # print("JPN Title :", titlej)
+    # print("Alt Titles :", alt_title, ",", title)
+
+
+def parse_dungeon(soup):
+    # The type of Dungeon (normal, special,etc)
+    dungeon_type = soup.body.find(class_='title value-end').get_text().strip()
+    type_split = ''.join([line.strip() for line in dungeon_type])
+
+    stats = soup.body.find_all(class_='title')
+    stat_vals = soup.body.find_all(class_="value-end")
+
+    # Stamina val
+    stam = soup.body.find(class_='blue').text
+
+    # Num of Battles
+    battles = soup.body.find(class_='green').text
+
+    # Correct dungeon typing becuase fuck it
+    if type_split == "SpecialDungeon":
+        type_split = "Special Dungeon"
+    elif type_split == "NormalDungeon":
+        type_split = "Normal Dungeon"
+
+    parsedDungeon['dungeonType'] = type_split
+    parsedDungeon['stamina'] = stam
+    parsedDungeon['battles'] = battles
+
+    # print("Dungeon Type :", type_split)
+    # print("Stamina :", stam)
+    # print("Battles :", battles)
+
+
+def parse_encounters(soup):
+    # Battle encounters
+    encounters = soup.body.find(id="tabledrop").find_all("tr")
+    floor = 0
+    # This method counts floors because I need to know how many encounter sets there are to know
+    # how many times to repeat the first set.
+    for item in encounters:
+        table1 = item.find_all("td")
+        for dat in table1:
+            attrs = dat.attrs
+            if 'class' in attrs:
+                if 'floorcontainer' in attrs['class']:
+                    floor += 1
+
+    num_repetitions = int(soup.body.find(class_='green').text) - floor + 1
+    num_rep_temp = num_repetitions
+    floor = 1
+    for item in encounters:
+        table1 = item.find_all("td")
+        for dat in table1:
+            attrs = dat.attrs
+            if 'class' in attrs:
+                if 'floorcontainer' in attrs['class']:
+                    if num_rep_temp > 1:
+                        print("Encounter Set", floor, ", Repeated", num_repetitions, " times.")
+                        floor += num_repetitions
+                        num_rep_temp = 0
+                    else:
+                        print("Encounter Set", floor)
+                        floor += 1
+
+                cardname = dat.find(class_='cardname')
+                if cardname is not None:
+                    print("\t", cardname.text)
+                    health = item.find(class_='nc bossHp')
+                    if health is not None:
+                        print('\t\tHP :', health.text)
+                    else:
+                        health = item.find(class_='nc')
+                        if health is not None:
+                            print('\t\tHP :', health.text)
+                    attack = item.find(class_='blue nc bossAtk')
+                    if attack is not None:
+                        print("\t\tAtk :", attack.text)
+                    else:
+                        attack = item.find(class_='blue nc')
+                        if attack is not None:
+                            print('\t\tAtk :', attack.text)
+                    defense = item.find(class_='green nc')
+                    if defense is not None:
+                        print('\t\tDEF :', defense.text)
+
+                    memo = item.find(class_='mmemodetail').find_all('a')
+                    for thing in memo:
+                        href = thing['href']
+                        if 'enemyskill' in href:
+                            # print('\t\t', href)
+                            parse_skill(href)
+
+
+def parse_skill(href):
+    base_link = "http://www.puzzledragonx.com/en/"
+    skill_link = base_link + href
+    site = urllib.request.urlopen(skill_link)
+    ss = bs(site, 'lxml')
+    skill = ss.find_all(class_='value-end')
+    print('\t\tSkill Name :', skill[0].text, ",", skill[2].text)
+    print('\t\t\tSkill Effect :', skill[1].text)
