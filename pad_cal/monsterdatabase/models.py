bf511a717d05c78a494dcad49b85195b62f7ed9b
Rohil <rohilthopu@gmail.com>
2018-07-11 23:56:43 -0500

    Restructured slightly to take advantage of inheritance, as an Active Skill is just a special Leader Skill

--- pad_cal/monsterdatabase/models.py
+++ pad_cal/monsterdatabase/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
 
-class ActiveSkill(models.Model):
-    name = models.CharField(max_length=200,default="",blank=True)
-    description = models.TextField(default="", blank=True,)
-    levels = models.IntegerField(default=0)
+class LeaderSkill(models.Model):
+    name = models.CharField(max_length=200, default="", blank=True)
+    description = models.TextField(default="", blank=True, )
     skillID = models.IntegerField()
     skillType = models.IntegerField()
+
+# An active skill is just a specialized leader skill that can only be activated ever so often.
+# As a result, inheritance is perfect here.
+class ActiveSkill(LeaderSkill):
+    levels = models.IntegerField(default=0)
     maxTurns = models.IntegerField()
     minTurns = models.IntegerField()
-
 
 
 # Create your models here.
 class Card(models.Model):
     activeSkill = models.OneToOneField(ActiveSkill, on_delete=models.CASCADE)
-
+    leaderSkill = models.OneToOneField(LeaderSkill, on_delete=models.CASCADE)
