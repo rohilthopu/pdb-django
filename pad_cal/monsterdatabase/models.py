0e4c2336f2d1fda06dbf0f0ba5139d313cf666ec
Rohil <rohilthopu@gmail.com>
2018-07-12 00:14:18 -0500

    Added a more flushed out model for a Card.

--- pad_cal/monsterdatabase/models.py
+++ pad_cal/monsterdatabase/models.py
@@ -4,18 +4,40 @@ from django.db import models
 class LeaderSkill(models.Model):
     name = models.CharField(max_length=200, default="", blank=True)
     description = models.TextField(default="", blank=True, )
-    skillID = models.IntegerField()
-    skillType = models.IntegerField()
+    skillID = models.IntegerField(blank=True)
+    skillType = models.IntegerField(blank=True)
 
 # An active skill is just a specialized leader skill that can only be activated ever so often.
 # As a result, inheritance is perfect here.
 class ActiveSkill(LeaderSkill):
     levels = models.IntegerField(default=0)
-    maxTurns = models.IntegerField()
-    minTurns = models.IntegerField()
+    maxTurns = models.IntegerField(blank=True)
+    minTurns = models.IntegerField(blank=True)
+
+
+class MonsterData(models.Model):
+    activeSkillID = models.IntegerField(blank=True)
+    attributeID = models.IntegerField()
+    baseID = models.IntegerField()
+    cardID = models.IntegerField()
+    cost = models.IntegerField()
+    leaderSkillID = models.IntegerField(blank=True)
+    maxATK = models.IntegerField()
+    maxHP = models.IntegerField()
+    maxLevel = models.IntegerField()
+    maxRCV = models.IntegerField()
+    minATK = models.IntegerField()
+    minHP = models.IntegerField()
+    minRCV = models.IntegerField()
+    name = models.CharField(default="", max_length=200)
+    rarity = models.IntegerField()
+
 
 
 # Create your models here.
-class Card(models.Model):
+class CardNA(models.Model):
     activeSkill = models.OneToOneField(ActiveSkill, on_delete=models.CASCADE)
     leaderSkill = models.OneToOneField(LeaderSkill, on_delete=models.CASCADE)
+    monster = models.OneToOneField(MonsterData, on_delete=models.CASCADE)
+
+
