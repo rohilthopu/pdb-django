65ca0b1c296aa1bb41a5a27ba18a76b78f69461c
Rohil Thopu <rohilthopu@gmail.com>
2018-08-02 15:03:48 -0500

    Potentially fixed issue with not properly setting upcoming to active. added guerrilla dungeon to admin panel

--- pad_db/guerrilladungeon/admin.py
+++ pad_db/guerrilladungeon/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from guerrilladungeon.models import GuerrillaDungeon
 # Register your models here.
+
+class GuerrillaDungeonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'startTime', 'endTime', 'server', 'group')
+
+
+admin.site.register(GuerrillaDungeon, GuerrillaDungeonAdmin)
