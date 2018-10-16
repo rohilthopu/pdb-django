cf60ac2253090b075541013752696fc78a43c15c
Rohil Thopu <rohilthopu@gmail.com>
2018-10-16 14:58:08 -0500

    added form widgets for styling

--- pad_db/issues/forms.py
+++ pad_db/issues/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Issue
 
+
 class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
-        
+        widgets = {
+            'server': forms.Select(),
+            'itemType': forms.Select(),
+            'itemName': forms.TextInput(attrs={'class': 'input'}),
+            'itemID': forms.TextInput(attrs={'class': 'input'}),
+            'description': forms.Textarea(attrs={'class': 'textarea'})
+        }
