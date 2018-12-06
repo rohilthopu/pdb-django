73efbb1a510fa7d75cc7f05d5a580a470fca2aad
Rohil Thopu <rohilthopu@gmail.com>
2018-12-05 18:43:53 -0600

    forgot to update max amount

--- pad_db/karmaleaderboard/management/commands/collectTopPosts.py
+++ pad_db/karmaleaderboard/management/commands/collectTopPosts.py
@@ -34,7 +34,7 @@ class Command(BaseCommand):
             for reply in comment.replies:
                 addCommentScore(reply)
 
-        for post in reddit.subreddit('puzzleanddragons').top(limit=10):
+        for post in reddit.subreddit('puzzleanddragons').top(limit=25):
 
             print("Collect karma for:", post.title)
 
