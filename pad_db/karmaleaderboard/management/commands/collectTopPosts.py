65a05d95c0eca4fa518dc5ef6310c93e5a72e638
Rohil Thopu <rohilthopu@gmail.com>
2018-12-18 01:42:16 -0600

    idk what i broke on the karma thing

--- pad_db/karmaleaderboard/management/commands/collectTopPosts.py
+++ pad_db/karmaleaderboard/management/commands/collectTopPosts.py
@@ -33,9 +33,9 @@ class Command(BaseCommand):
             for reply in comment.replies:
                 addCommentScore(reply)
 
-        for post in reddit.subreddit('puzzleanddragons').top(limit=250):
+        for post in reddit.subreddit('puzzleanddragons').top(limit=500):
 
-            print("Collect karma for:", post.title)
+            # print("Collect karma for:", post.title)
 
             addScore(post)
 
