adbdd703b7568e122d3d856b633d69d67f84a36f
Rohil Thopu <rohilthopu@gmail.com>
2019-01-29 17:03:36 -0600

    handle None cases

--- pad_db/dungeon/management/commands/updatedungeons.py
+++ pad_db/dungeon/management/commands/updatedungeons.py
@@ -37,15 +37,16 @@ class Command(BaseCommand):
                 fl.waves = floor.waves
                 fl.possibleDrops = json.dumps(floor.possible_drops)
                 fl.entryRequirement = floor.entry_requirement if floor.entry_requirement is not None else "None"
-                fl.requiredDungeon = floor.required_dungeon
+                fl.requiredDungeon = floor.required_dungeon if floor.required_dungeon is not None else "None"
                 fl.remainingModifiers = json.dumps(floor.remaining_modifiers)
                 fl.teamModifiers = json.dumps(floor.team_modifiers)
                 fl.encounterModifiers = json.dumps(floor.modifiers_clean)
-                fl.enhancedType = floor.enhanced_type
-                fl.enhancedAttribute = floor.enhanced_attribute
+
+                fl.enhancedType = floor.enhanced_type if floor.enhanced_type is not None else "None"
+                fl.enhancedAttribute = floor.enhanced_attribute if floor.enhanced_attribute is not None else "None"
                 fl.messages = json.dumps(floor.messages)
                 fl.fixedTeam = json.dumps(floor.fixed_team)
-                fl.score = floor.score
+                fl.score = floor.score if floor.score is not None else 0
                 fl.save()
 
         self.stdout.write(self.style.SUCCESS('Starting NA DUNGEON DB update.'))
