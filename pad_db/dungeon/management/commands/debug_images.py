160f205e4637e9b60d6047a69ddf5d63ace3b35b
Rohil Thopu <rohilthopu@gmail.com>
2019-03-27 17:01:34 -0500

    fixed mini section that collects the image id's for dungeons/dungeon floors

--- pad_db/dungeon/management/commands/debug_images.py
+++ pad_db/dungeon/management/commands/debug_images.py
@@ -12,8 +12,18 @@ class Command(BaseCommand):
 
         for dungeon in dungeons:
             encounter_data = encounters.filter(dungeon_id=dungeon.dungeonID)
+            dungeon_floors = floors.filter(dungeonID=dungeon.dungeonID)
+
             if encounter_data.last() is not None:
                 boss = encounter_data.last().encounter_data
 
                 card_id = json.loads(boss)[-1]['card_id']
-                print(card_id)
+                dungeon.imageID = card_id
+                dungeon.save()
+
+                for floor in dungeon_floors:
+                    boss = encounter_data.filter(floor_id=floor.floorNumber)
+                    if boss is not None and boss.last() is not None:
+                        card_id = json.loads(boss.last().encounter_data)[-1]['card_id']
+                        floor.imageID = card_id
+                        floor.save()
--- pad_db/dungeon/management/commands/updatedungeons.py
+++ pad_db/dungeon/management/commands/updatedungeons.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 import json
-from dungeon.models import Dungeon, Floor
+from dungeon.models import Dungeon, Floor, EncounterSet
 from dataversions.models import Version
 import time
 from .dungeon_parser.dungeon_parser import get_dungeon_list
@@ -9,14 +9,13 @@ from .dungeon_parser.dungeon_parser import get_dungeon_list
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        def make_dungeon_from_object(dungeon, image_id):
+        def make_dungeon_from_object(dungeon):
             if "*" not in dungeon.clean_name:
                 new_dungeon = Dungeon()
                 new_dungeon.name = dungeon.clean_name
                 new_dungeon.dungeonID = dungeon.dungeon_id
                 new_dungeon.floorCount = len(dungeon.floors)
                 new_dungeon.dungeonType = dungeon.alt_dungeon_type
-                new_dungeon.imageID = image_id[0] if len(image_id) > 0 else 0
                 new_dungeon.save()
 
         def make_floor_from_object(floors, dungeon_id):
@@ -40,10 +39,6 @@ class Command(BaseCommand):
                 fl.messages = json.dumps(floor.messages)
                 fl.fixedTeam = json.dumps(floor.fixed_team)
                 fl.score = floor.score if floor.score is not None else 0
-
-                # Just use the last drop for the image id for now.
-                image_id = [*floor.possible_drops]
-                fl.imageID = image_id[0] if len(image_id) > 0 else 0
                 fl.save()
 
         self.stdout.write(self.style.SUCCESS('Starting NA DUNGEON DB update.'))
@@ -59,13 +54,36 @@ class Command(BaseCommand):
 
         for item in dungeon_list:
 
-            make_dungeon_from_object(item, [*item.floors[-1].possible_drops])
+            make_dungeon_from_object(item)
 
             if '*' not in item.clean_name:
                 make_floor_from_object(item.floors, item.dungeon_id)
 
+        encounters = EncounterSet.objects.all()
+        dungeons = Dungeon.objects.all()
+        floors = Floor.objects.all()
+
+        for dungeon in dungeons:
+            encounter_data = encounters.filter(dungeon_id=dungeon.dungeonID)
+            dungeon_floors = floors.filter(dungeonID=dungeon.dungeonID)
+
+            if encounter_data.last() is not None:
+                boss = encounter_data.last().encounter_data
+
+                card_id = json.loads(boss)[-1]['card_id']
+                dungeon.imageID = card_id
+                dungeon.save()
+
+                for floor in dungeon_floors:
+                    boss = encounter_data.filter(floor_id=floor.floorNumber)
+                    if boss is not None and boss.last() is not None:
+                        card_id = json.loads(boss.last().encounter_data)[-1]['card_id']
+                        floor.imageID = card_id
+                        floor.save()
+
         end = time.time()
         self.stdout.write(self.style.SUCCESS('NA DUNGEON update complete.'))
+
         print("Elapsed time :", end - start)
 
         print("Updating version")
