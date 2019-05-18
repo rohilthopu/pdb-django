73dbe27e9c02c238ab19054faf7a6c2034cfa388
Rohil Thopu <rohilthopu@gmail.com>
2019-05-18 15:15:22 -0500

    moved the processed dir location to the settings file

--- pad_db/guerrilla/views.py
+++ pad_db/guerrilla/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import os
+from django.conf import settings
 
 
 # Load from file for mobile app
 def guerrilla_view(request):
-    file_dir = '/Users/rohil/projects/personal/pdb_processor/output/{}'
-
     file_name = 'guerrilla_data.json'
-    with open(os.path.abspath(file_dir.format(file_name)), 'r') as f:
+    with open(os.path.abspath(settings.PROCESSED_FILE_DIR.format(file_name)), 'r') as f:
         data = json.load(f)
 
     return JsonResponse(data, safe=False)
