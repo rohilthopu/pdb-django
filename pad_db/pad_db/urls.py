bd9f325dfebddc01eb3e9a9361be4dabfe046cd1
Rohil Thopu <rohilthopu@gmail.com>
2019-05-18 15:19:07 -0500

    removed unnecessary spaces in the urls file, added new api endpoint for monsters

--- pad_db/pad_db/urls.py
+++ pad_db/pad_db/urls.py
@@ -31,40 +31,43 @@ from dataversions import apiviews as dvav
 from dungeon import apiviews as dav
 
 from guerrilla import views as guerrilla_views
+from monsters import views as monster_views
+
 
 urlpatterns = [
-                  url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 
-                  # Guerrilla Dungeons
-                  path('', gv.DungeonView),
+    # Guerrilla Dungeons
+    path('', gv.DungeonView),
 
-                  # NA Items
-                  path('monsterdb/na/', mv.cardList),
-                  path('monster/na/<int:card_id>/', mv.cardView),
-                  path('monster/na/edit/<int:card_id>/', mv.editCardView),
-                  path('dungeons/na/', dv.dungeonListView),
-                  path('dungeons/na/<int:d_id>/', dv.dungeonView),
-                  path('activeskills/na/', mv.activeSkillListView),
-                  path('activeskills/na/<int:skill_id>/', mv.activeSkillView),
-                  path('leaderskills/na', mv.leaderSkillListView),
-                  path('leaderskills/na/<int:skill_id>/', mv.leaderSkillView),
-                  path('leaderskills/na/edit/<int:skill_id>/', mv.editLeaderSkill),
-                  path('activeskills/na/edit/<int:skill_id>/', mv.editActiveSkill),
+    # NA Items
+    path('monsterdb/na/', mv.cardList),
+    path('monster/na/<int:card_id>/', mv.cardView),
+    path('monster/na/edit/<int:card_id>/', mv.editCardView),
+    path('dungeons/na/', dv.dungeonListView),
+    path('dungeons/na/<int:d_id>/', dv.dungeonView),
+    path('activeskills/na/', mv.activeSkillListView),
+    path('activeskills/na/<int:skill_id>/', mv.activeSkillView),
+    path('leaderskills/na', mv.leaderSkillListView),
+    path('leaderskills/na/<int:skill_id>/', mv.leaderSkillView),
+    path('leaderskills/na/edit/<int:skill_id>/', mv.editLeaderSkill),
+    path('activeskills/na/edit/<int:skill_id>/', mv.editActiveSkill),
 
-                  path('leaderboard', kv.leaderboardView),
+    path('leaderboard', kv.leaderboardView),
 
-                  # API views
-                  path('api/guerrilla/', gav.GuerrillaList.as_view()),
-                  path('api/monsters/na/', mav.MonsterList.as_view()),
-                  path('api/skills/na/', mav.SkillList.as_view()),
-                  path('api/leaderboard/', kav.LeaderboardList.as_view()),
-                  path('api/version/', dvav.VersionList.as_view()),
-                  path('api/dungeons/', dav.DungeonList.as_view()),
-                  path('api/floors/', dav.FloorList.as_view()),
-                  path('api/enemyskills/', mav.EnemySkillList.as_view()),
-                  path('api/encounters/', dav.EncounterList.as_view()),
+    # API views
+    path('api/guerrilla/', gav.GuerrillaList.as_view()),
+    path('api/monsters/na/', mav.MonsterList.as_view()),
+    path('api/skills/na/', mav.SkillList.as_view()),
+    path('api/leaderboard/', kav.LeaderboardList.as_view()),
+    path('api/version/', dvav.VersionList.as_view()),
+    path('api/dungeons/', dav.DungeonList.as_view()),
+    path('api/floors/', dav.FloorList.as_view()),
+    path('api/enemyskills/', mav.EnemySkillList.as_view()),
+    path('api/encounters/', dav.EncounterList.as_view()),
 
-                # new api endpoints
-                path('api/guerrilla_dungeons/', guerrilla_views.guerrilla_view)
+    # new api endpoints
+    path('api/guerrilla_dungeons/', guerrilla_views.guerrilla_view),
+    path('api/monsters/', monster_views.monsters_view),
 
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
