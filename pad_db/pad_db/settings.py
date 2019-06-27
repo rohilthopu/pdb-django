05b76f55015b455a8b1122e7a9c10738bf02ec72
Rohil Thopu <rohilthopu@gmail.com>
2019-06-27 14:04:00 -0500

    folder cleanup

--- pad_db/pad_db/settings.py
+++ pad_db/pad_db/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'maintenance_mode.middleware.MaintenanceModeMiddleware',
 ]
 
 ROOT_URLCONF = 'pad_db.urls'
--- pad_db/pad_db/urls.py
+++ pad_db/pad_db/urls.py
@@ -1,6 +1,4 @@
-from django.conf.urls import url
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from monsters import views as mv
