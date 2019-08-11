ea7a39a5220f795cc80fdea7f7ba0443bcedb6e8
Rohil Thopu <rohilthopu@gmail.com>
2019-08-11 15:51:47 -0500

    change the rest endpoints to have a /rest/ prefix and added a basic view to fetch a monster from es using a redirect

--- pad_db/data_manager/views.py
+++ pad_db/data_manager/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.shortcuts import redirect
 from .search import query_es
 
 
@@ -12,3 +13,6 @@ def search(request, query):
     search_results = [result.to_dict() for result in query_results]
 
     return JsonResponse(search_results, safe=False, json_dumps_params={'indent': 4})
+
+def get_monster_by_id(request, card_id):
+    return redirect('/search/card_id={}'.format(card_id))
--- pad_db/pad_db/urls.py
+++ pad_db/pad_db/urls.py
@@ -7,20 +7,22 @@ from guerrilla import apiviews as gav
 from monsters import apiviews as mav
 from skills import apiviews as sav
 from dungeons import apiviews as dav
-from data_manager import views as sv
+
+from data_manager import views as search_api
 
 urlpatterns = [
     # new api endpoints
-    path('monster/<int:card_id>/', mav.MonsterObject.as_view()),
-    path('monsters/', mav.MonsterList.as_view()),
-    path('skills/', sav.SkillList.as_view()),
-    path('skill/<int:skill_id>/', sav.SkillObject.as_view()),
-    path('guerrilla/', gav.GuerrillaList.as_view()),
-    path('dungeons/', dav.DungeonList.as_view()),
-    path('dungeon/<int:dungeon_id>/', dav.DungeonObject.as_view()),
-    path('floors/', dav.AllFloorsList.as_view()),
-    path('floors/<int:dungeon_id>/', dav.FloorList.as_view()),
-    path('floor/<int:dungeon_id>/<int:floor_number>/', dav.FloorObject.as_view()),
-    path('search/<str:query>/', sv.search),
+    path('rest/monster/<int:card_id>/', mav.MonsterObject.as_view()),
+    path('rest/monsters/', mav.MonsterList.as_view()),
+    path('rest/skills/', sav.SkillList.as_view()),
+    path('rest/skill/<int:skill_id>/', sav.SkillObject.as_view()),
+    path('rest/guerrilla/', gav.GuerrillaList.as_view()),
+    path('rest/dungeons/', dav.DungeonList.as_view()),
+    path('rest/dungeon/<int:dungeon_id>/', dav.DungeonObject.as_view()),
+    path('rest/floors/', dav.AllFloorsList.as_view()),
+    path('rest/floors/<int:dungeon_id>/', dav.FloorList.as_view()),
+    path('rest/floor/<int:dungeon_id>/<int:floor_number>/', dav.FloorObject.as_view()),
+    path('search/<str:query>/', search_api.search),
+    path('monster/<int:card_id>/', search_api.get_monster_by_id),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
