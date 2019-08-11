d22555789c9f4fb8fc640a8f36a00c1c8cd04662
Rohil Thopu <rohilthopu@gmail.com>
2019-08-11 11:53:15 -0500

    moved constants into their own file

--- pad_db/data_manager/maps.py
+++ pad_db/data_manager/maps.py
@@ -1,3 +1,9 @@
+
+OPERATORS = ['>=', '<=', '=', '>', '<']
+INDICES = ['skills', 'monsters']
+COLUMNS = ['CARD_ID', 'NAME']
+
+
 AWAKENINGS = {
     '': 0,
     'Enhanced HP': 1,
@@ -68,3 +74,76 @@ AWAKENINGS = {
     'Reduced Attack': 66,
     'Reduced RCV': 67,
 }
+
+AWAKENING_ALIASES = {
+    '7c': 43,
+    '10c': 61,
+    'unbindable': 10,
+    'unbindable+': 52,
+    'autoheal': 9,
+    'blind resist': 10,
+    'jammer resist': 12,
+    'poison resist': 13,
+    'time extend': 19,
+    'te': 19,
+    'bind clear': 20,
+    'sb': 21,
+    'tpa': 27,
+    'prong': 27,
+    'skill bind resist': 28,
+    'sbr': 28,
+    'heal prong': 29,
+    'fua': 45,
+    'follow up attack': 45,
+    'followup attack': 45,
+    'team hp+': 46,
+    'team rcv+': 46,
+    'super fua': 50,
+    'fua2': 50,
+    'fua 2': 50,
+    'vdp': 48,
+    'dvp': 48,
+    'void damage piercer': 48,
+    'equip': 49,
+    'time extend+': 53,
+    'te+': 53,
+    'tape resist': 55,
+    'sb+': 56,
+    'gt 80%': 57,
+    'lt 50%': 58,
+    'l unlock': 59,
+    'l-unlock': 59,
+    'l attack': 60,
+    'l-attack': 60,
+    'hp down': 65,
+    'atk down': 66,
+    'rcv down': 67,
+    'poison resist+': 70,
+    'jammer resist+': 71,
+    'blind resist+': 72,
+}
+
+ATTRIBUTE_ALIASES = {
+    'hpmf': 'leader_skill.hp_mult_full',
+    'atkmf': 'leader_skill.atk_mult_full',
+    'rcvmf': 'leader_skill.rcv_mult_full',
+    'hpm': 'leader_skill.hp_mult',
+    'atkm': 'leader_skill.atk_mult',
+    'rcvm': 'leader_skill.rcv_mult',
+    'leader_shield': 'leader_skill.shield',
+    'pair_shield': 'leader_skill.shield_full',
+    'active_hpm': 'active_skill.hp_mult',
+    'active_atkm': 'active_skill.atk_mult',
+    'active_rcvm': 'active_skill.rcv_mult',
+    'active_shield': 'active_skill.shield',
+    'has_evomat': 'evolution_materials',
+    'has_devomat': 'un_evolution_materials',
+    'has_evomat_raw': 'evolution_materials_raw',
+    'has_devomat_raw': 'un_evolution_materials_raw',
+}
+
+LEADER_SKILL_VALUES = {'leader_skill.hp_mult_full', 'leader_skill.rcv_mult_full', 'leader_skill.rcv_mult', 'leader_skill.shield_full',
+                       'leader_skill.atk_mult', 'leader_skill.atk_mult_full', 'leader_skill.shield', 'leader_skill.hp_mult'}
+
+ACTIVE_SKILL_VALUES = {'active_skill.atk_mult', 'active_skill.shield',
+                       'active_skill.rcv_mult', 'active_skill.hp_mult'}
--- pad_db/data_manager/search.py
+++ pad_db/data_manager/search.py
@@ -1,92 +1,13 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 from prettytable import PrettyTable
-
 # this just allows me to run the script outside of the djano env for testing
 try:
-    from .maps import AWAKENINGS
+    from .maps import AWAKENINGS, AWAKENING_ALIASES, ATTRIBUTE_ALIASES, LEADER_SKILL_VALUES, ACTIVE_SKILL_VALUES, OPERATORS, COLUMNS, INDICES
 except:
-    from maps import AWAKENINGS
+    from maps import AWAKENINGS, AWAKENING_ALIASES, ATTRIBUTE_ALIASES, LEADER_SKILL_VALUES, ACTIVE_SKILL_VALUES, OPERATORS, COLUMNS, INDICES
 
 client = Elasticsearch('https://elastic.pad-db.com')
-
-operators = ['>=', '<=', '=', '>', '<']
-int_attributes = ['awakenings', 'evolutions']
-indices = ['skills', 'monsters']
-COLUMNS = ['CARD_ID', 'NAME']
-
-AWAKENING_ALIASES = {
-    '7c': 43,
-    '10c': 61,
-    'unbindable': 10,
-    'unbindable+': 52,
-    'autoheal': 9,
-    'blind resist': 10,
-    'jammer resist': 12,
-    'poison resist': 13,
-    'time extend': 19,
-    'te': 19,
-    'bind clear': 20,
-    'sb': 21,
-    'tpa': 27,
-    'prong': 27,
-    'skill bind resist': 28,
-    'sbr': 28,
-    'heal prong': 29,
-    'fua': 45,
-    'follow up attack': 45,
-    'followup attack': 45,
-    'team hp+': 46,
-    'team rcv+': 46,
-    'super fua': 50,
-    'fua2': 50,
-    'fua 2': 50,
-    'vdp': 48,
-    'dvp': 48,
-    'void damage piercer': 48,
-    'equip': 49,
-    'time extend+': 53,
-    'te+': 53,
-    'tape resist': 55,
-    'sb+': 56,
-    'gt 80%': 57,
-    'lt 50%': 58,
-    'l unlock': 59,
-    'l-unlock': 59,
-    'l attack': 60,
-    'l-attack': 60,
-    'hp down': 65,
-    'atk down': 66,
-    'rcv down': 67,
-    'poison resist+': 70,
-    'jammer resist+': 71,
-    'blind resist+': 72,
-}
-
-ATTRIBUTE_ALIASES = {
-    'hpmf': 'leader_skill.hp_mult_full',
-    'atkmf': 'leader_skill.atk_mult_full',
-    'rcvmf': 'leader_skill.rcv_mult_full',
-    'hpm': 'leader_skill.hp_mult',
-    'atkm': 'leader_skill.atk_mult',
-    'rcvm': 'leader_skill.rcv_mult',
-    'leader_shield': 'leader_skill.shield',
-    'pair_shield': 'leader_skill.shield_full',
-    'active_hpm': 'active_skill.hp_mult',
-    'active_atkm': 'active_skill.atk_mult',
-    'active_rcvm': 'active_skill.rcv_mult',
-    'active_shield': 'active_skill.shield',
-    'has_evomat': 'evolution_materials',
-    'has_devomat': 'un_evolution_materials',
-    'has_evomat_raw': 'evolution_materials_raw',
-    'has_devomat_raw': 'un_evolution_materials_raw',
-}
-
-LEADER_SKILL_VALUES = {'leader_skill.hp_mult_full', 'leader_skill.rcv_mult_full', 'leader_skill.rcv_mult', 'leader_skill.shield_full',
-                       'leader_skill.atk_mult', 'leader_skill.atk_mult_full', 'leader_skill.shield', 'leader_skill.hp_mult'}
-
-ACTIVE_SKILL_VALUES = {'active_skill.atk_mult', 'active_skill.shield',
-                       'active_skill.rcv_mult', 'active_skill.hp_mult'}
 
 
 def update_awakening_map():
@@ -312,7 +233,7 @@ def query_greater_than(es_search: Search, operator: str, attribute: str, value: 
 
 
 def get_operator(query_part: str):
-    for operator in operators:
+    for operator in OPERATORS:
         if operator in query_part:
             return operator
     return None
@@ -460,7 +381,7 @@ def query_es(query_str: str):
     raw_query = query_str
 
     # make sure the index exists (if i ever allow non monster centered queries)
-    if index in indices:
+    if index in INDICES:
         print()
         print('Searching {} index'.format(index.upper()))
         print('Desired index: {}'.format(index))
@@ -492,7 +413,7 @@ def test_raw_query():
     index = 'monsters'
     # raw_query = 'has evomat = machine athena gem'
 
-    if index in indices:
+    if index in INDICES:
         print()
         print('Searching {} index'.format(index.upper()))
         print('Desired index: {}'.format(index))
