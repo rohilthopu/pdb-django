d9b8b894a80d3860ed65a34871fb798db51add01
Rohil Thopu <rohilthopu@gmail.com>
2019-08-11 14:56:25 -0500

    remove templates

--- pad_db/pad_db/settings.py
+++ pad_db/pad_db/settings.py
@@ -53,23 +53,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pad_db.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates/na'),
-                 os.path.join(BASE_DIR, 'templates/../../deprecated/jp')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'pad_db.wsgi.application'
 
 # Database
