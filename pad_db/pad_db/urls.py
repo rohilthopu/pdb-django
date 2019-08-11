e8c24534b4d51243049f9ac94bf008c59e83f98d
Rohil Thopu <rohilthopu@gmail.com>
2019-08-11 14:41:44 -0500

    rename some endpoints

--- pad_db/pad_db/urls.py
+++ pad_db/pad_db/urls.py
@@ -11,16 +11,16 @@ from data_manager import views as sv
 
 urlpatterns = [
     # new api endpoints
-    path('api/monster/<int:card_id>/', mav.MonsterObject.as_view()),
-    path('api/monsters/', mav.MonsterList.as_view()),
-    path('api/skills/', sav.SkillList.as_view()),
-    path('api/skill/<int:skill_id>/', sav.SkillObject.as_view()),
-    path('api/guerrilla/', gav.GuerrillaList.as_view()),
-    path('api/dungeons/', dav.DungeonList.as_view()),
-    path('api/dungeon/<int:dungeon_id>/', dav.DungeonObject.as_view()),
-    path('api/floors/', dav.AllFloorsList.as_view()),
-    path('api/floors/<int:dungeon_id>/', dav.FloorList.as_view()),
-    path('api/floor/<int:dungeon_id>/<int:floor_number>/', dav.FloorObject.as_view()),
+    path('monster/<int:card_id>/', mav.MonsterObject.as_view()),
+    path('monsters/', mav.MonsterList.as_view()),
+    path('skills/', sav.SkillList.as_view()),
+    path('skill/<int:skill_id>/', sav.SkillObject.as_view()),
+    path('guerrilla/', gav.GuerrillaList.as_view()),
+    path('dungeons/', dav.DungeonList.as_view()),
+    path('dungeon/<int:dungeon_id>/', dav.DungeonObject.as_view()),
+    path('floors/', dav.AllFloorsList.as_view()),
+    path('floors/<int:dungeon_id>/', dav.FloorList.as_view()),
+    path('floor/<int:dungeon_id>/<int:floor_number>/', dav.FloorObject.as_view()),
     path('search/<str:query>/', sv.search),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
