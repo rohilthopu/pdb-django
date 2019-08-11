a908cd4b0ca9f208047b8abddd117e6f88dd96cf
Rohil Thopu <rohilthopu@gmail.com>
2019-08-11 15:44:30 -0500

    changed search response to be one item if searching specific, otherwise return list

--- pad_db/data_manager/views.py
+++ pad_db/data_manager/views.py
@@ -5,5 +5,10 @@ from .search import query_es
 def search(request, query):
     query_results = query_es(query)
 
+
+    if len(query_results) == 1:
+        return JsonResponse(query_results[0].to_dict(), json_dumps_params={'indent': 4})
+
     search_results = [result.to_dict() for result in query_results]
+
     return JsonResponse(search_results, safe=False, json_dumps_params={'indent': 4})
