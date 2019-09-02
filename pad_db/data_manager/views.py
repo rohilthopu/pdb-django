84c9435dabe63a7b61ba2f6747e92d3c38a88468
Rohil Thopu <rohilthopu@gmail.com>
2019-09-02 15:52:17 -0500

    Revert "remove poorly designed code lol"
    This reverts commit 99b4fc031069b0bdfa46d937781af77f51c401e3.

--- pad_db/data_manager/views.py
+++ pad_db/data_manager/views.py
@@ -5,7 +5,12 @@ from .search import query_es
 
 def search(request, index, query):
     query_results = query_es(index, query)
+
+    if len(query_results) == 1:
+        return JsonResponse(query_results[0].to_dict(), json_dumps_params={'indent': 4})
+
     search_results = [result.to_dict() for result in query_results]
+
     return JsonResponse(search_results, safe=False, json_dumps_params={'indent': 4})
 
 
